040ce87581dfa0d06ccc500723852a791431ee22
Cbuckles17 <cbuckle3@gmu.edu>
2019-06-25 11:17:23 -0500

    Adding adjacent_elements_product problem in GOLang

---
+++ intro/edge_of_the_ocean/adjacent_elements_product/adjacent_elements_product.go
@@ -0,0 +1,12 @@
+func adjacentElementsProduct(inputArray []int) int {
+    max := inputArray[0] * inputArray[1]
+    
+    for i := 1; i < len(inputArray) - 1; i++ {
+        //fmt.Println(i, inputArray[i], inputArray[i + 1], max)
+        if inputArray[i] * inputArray[i + 1] > max {
+            max = inputArray[i] * inputArray[i + 1]
+        }
+    }
+    
+    return max
+}
